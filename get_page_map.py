6e9a11917bf4b1fd74e2b6b2b2792349dd7e7483
Adrian <adrianl24.al@gmail.com>
2023-04-13 21:47:33 +0200

    Improved logic of main part. Still don't work in 100% properly.

--- get_page_map.py
+++ get_page_map.py
@@ -4,6 +4,7 @@ from lxml import html, etree
 
 page_url = "https://www.softwarestudio.com.pl"
 main_url_name = "softwarestudio.com.pl"
+baza_rozszerzen = ["jpg", "svg", "png", "ebp", ".js", "peg"]
 
 
 def get_full_links_base(html_str):
@@ -27,50 +28,66 @@ def get_full_links_base(html_str):
     
     clean_links_base = []
     for i_link in links_main_base:
-        if i_link[-3:] != "jpg" and i_link[-3:] != "svg" and i_link[-3:] != "png" and i_link[-3:] != "ebp" and i_link[-3:] != ".js" and i_link[-3:] != "peg":
+        if i_link[-3:] not in baza_rozszerzen:
             if "\\\\" not in i_link:
                 clean_links_base.append(i_link)
     return clean_links_base
 
-
-all_links = []
-check_links = []
-bad_links = []
-check_links.append(page_url)
-while check_links != []:
-    for i in check_links:
-        if i in bad_links:
-            check_links.remove(i)
+def get_html_str(url_to_get_page):
     try:
-        check_link = check_links[0]
+        request = requests.get(url_to_get_page)
+        if request.status_code == 200:
+            page = etree.HTML(request.content)
+            html_in_bits = etree.tostring(page)
+            html_in_str = str(html_in_bits)
+            if "<" in html_in_str:
+                return html_in_str
+            else:
+                return ""
+        else:
+            return ""
     except:
-        continue
-    
-    check_links.remove(check_link)
-    bad_links.append(check_link)
-    try:
-        request = requests.get(check_link)
-    except:
-        continue
-    if request.status_code == 200:
-        all_links.append(check_link)
-        page = etree.HTML(request.content)
-        html_in_bits = etree.tostring(page)
-        html_in_str = str(html_in_bits)
-        
-        clean_links = get_full_links_base(html_in_str)
+        return ""
 
-        for link in clean_links:
-            if link not in all_links and link not in check_links:
-                check_links.append(link)
-                bad_links.append(link)
-
+urls_holder = [page_url]
+wrong_url = []
+good_url = []
+i = 0
+while urls_holder != []:
+    last_url = urls_holder[0]
+    html_str_in_processing = get_html_str(last_url)
+    if html_str_in_processing == "":
+        wrong_url.append(last_url)
+        urls_holder.remove(last_url)
     else:
-        print("Request fail!" + check_link)
-        bad_links.append(check_link)
+        if last_url not in good_url:
+            good_url.append(last_url)
+             
+        holder = get_full_links_base(html_str_in_processing)
+        # if i == 40:
+        #     print(holder)
+        for link in holder:
+            if link not in urls_holder:
+                urls_holder.append(link)
+                
+        for link in urls_holder:
+            if link in wrong_url or link in good_url:
+                urls_holder.remove(link)
+    
+    i+=1
+    if i == 100:
+        print(urls_holder)
+        break
+    
+    urls_holder = list(dict.fromkeys(urls_holder))
+    print(len(urls_holder))
+    print(len(good_url))
+    
 
-    print(len(check_links))
-    print(len(all_links))
+print(good_url)
+print(len(good_url))
 
-print(all_links)
-print(check_links)
+
+
+
+
